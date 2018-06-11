772d09f5330068e806583e9f03178416763a391c
kb <tir3su@gmail.com>
2018-06-11 23:30:40 +0900

    MainAimのジャミング効果の実装(大きくなったり小さくなったり) & PlayerのDebugテキスト表示の画面追従スクリプト

---
+++ JummingAim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JummingAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    
+	// Update is called once per frame
+	void Update () {
+        float t = Time.time * 2;
+        /*
+        // 1 - 2
+        this.transform.localScale = new Vector3(1 + Mathf.Pow(Mathf.PingPong(t, 1f), 4), 1 + Mathf.Pow(Mathf.PingPong(t, 1f), 4), 1);
+        // -0.75 - -1.5
+        this.transform.localPosition = new Vector3(-0.75f - Mathf.Pow(Mathf.PingPong(t, 1f), 4) * 0.75f, 0, 0);
+        */
+        // 1 - 2
+        this.transform.localScale = new Vector3(0.5f + Mathf.Pow(Mathf.PingPong(t, 1f), 4) * 1.5f, 0.5f + Mathf.Pow(Mathf.PingPong(t, 1f), 4) * 1.5f, 1);
+        // -0.75 - -1.5
+        this.transform.localPosition = new Vector3(-0.4f - Mathf.Pow(Mathf.PingPong(t, 1f), 4)  * 1.1f, 0, 0);
+    }
+}
---
+++ PlayerDbgTextPos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDbgTextPos : MonoBehaviour {
+
+    GameObject playerObj;
+    RectTransform thisRectTransform;
+    Vector2 offset = new Vector2(-50f, 0);
+
+	// Use this for initialization
+	void Start () {
+        thisRectTransform = GetComponent<RectTransform>();
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisRectTransform.position = RectTransformUtility.WorldToScreenPoint(Camera.main, playerObj.transform.position) + offset;
+    }
+}
