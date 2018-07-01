4a226abd7766f7e6877f5fc59d0b100870f2a3b0
kb <tir3su@gmail.com>
2018-07-02 03:28:28 +0900

    雷撃のアニメーションステート状態でゲームオブジェクトのON/OFF切替

---
+++ LightningAutoPlayDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningAutoPlayDestroy : MonoBehaviour {
+
+    private Animator anim;
+    public GameObject lightning;
+    public GameObject energyLightning;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        lightning.SetActive(false);
+        energyLightning.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        AnimatorStateInfo nextInfo = anim.GetNextAnimatorStateInfo(0);
+        if(stateInfo.fullPathHash == Animator.StringToHash("Base Layer.Lightning.ChantPattern02finish"))
+        {
+            lightning.SetActive(false);
+            energyLightning.SetActive(false);
+        }
+        else if(nextInfo.fullPathHash == Animator.StringToHash("Base Layer.Lightning.ChantPattern02in"))
+        {
+            lightning.SetActive(true);
+            energyLightning.SetActive(true);
+        }
+	}
+}
