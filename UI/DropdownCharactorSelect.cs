0a548ffdf66313d61a1f43b4b994dfa89eff4b30
kb <tir3su@gmail.com>
2018-08-24 06:22:30 +0900

    Titleシーンの追加、プレイヤーの選択のドロップメニューの追加

---
+++ UI/DropdownCharactorSelect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownCharactorSelect : MonoBehaviour {
+
+    /// <summary> ドロップダウンメニュー /// </summary>
+    [SerializeField]
+    private Dropdown dropdown;
+    /// <summary> デバッグ用テキスト /// </summary>
+    [SerializeField]
+    private Text text;
+    /// <summary> PlayerObject /// </summary>
+    [SerializeField]
+    private GameObject SDunitychanPrefab;
+    [SerializeField]
+    private GameObject SDmisakichanPrefab;
+
+    private void Awake()
+    {
+        // 初期値はSDunitychanで設定
+        GameSystem.Instance.PlayerName = "SDunitychan";
+    }
+
+    /// <summary> ドロップダウンイベント時に呼び出される関数 /// </summary>
+    /// <param name="result"></param>
+    public void DropdownChanged(int result)
+    {
+        // デバッグ用
+        text.text = dropdown.captionText.text;
+
+        // ドロップダウンメニューで選択された文字列からプレイヤーオブジェクトのセット
+        GameSystem.Instance.PlayerName = dropdown.captionText.text;
+    }
+}
---
+++ UI/TitleStartButton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleStartButton : MonoBehaviour {
+    public void TitleStartButtonOn() {
+        SceneManager.LoadScene("MainScene");
+    }
+}
